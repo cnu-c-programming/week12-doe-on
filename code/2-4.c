eb5cb66660cd78d9fde1fae108e4ec049ba56686
doe-on <fovusshi01197@gmail.com>
2026-05-28 14:18:01 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,6 +6,22 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    int count[26] = {0};
+    char ch;
+
+    while ((ch = fgetc(fp)) != EOF) {
+
+        if (ch >= 'a' && ch <= 'z')
+        count[ch - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+
+        printf("%c: %d\n",
+               'a' + i,
+               count[i]);
+    }
+
 
 
     fclose(fp);
