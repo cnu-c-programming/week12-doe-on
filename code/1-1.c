816933dffe10e506f2a3fcda5c80a49ed8ab5dfd
doe-on <fovusshi01197@gmail.com>
2026-05-28 13:05:56 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -5,17 +5,17 @@ int main(int argc, const char* argv[]) {
     if (argc < 2)
         return 0;
 
-    const char* filenmae = arge[1];
+    const char* filenmae = argv[1];
     char buffer[256] = {0};
 
     FILE* fp = fopen(filename, "r");
-    if (fp== NULL)
+    if (fp == NULL)
         return 0;
 
     fgets(buffer, sizedof(buffer), fp):
     fclose(fp);
 
-    printf("%s\n", buffer):
+    printf("%s\n", buffer);
 
     return 0;
 }
