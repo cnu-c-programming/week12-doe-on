2f48f9c6d524a445ff27dab670e8b4aec5c85e6d
doe-on <fovusshi01197@gmail.com>
2026-05-28 18:17:50 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,46 +1,15 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-typedef struct config {
-
-    char InputFileName[64];
-    int Options;
-    char SectionName[64];
-    unsigned long long Address;
-
-} Config;
-
-void config_parser(Config* config_ptr) {
-
-    FILE* fp = fopen("config.txt", "r");
-
-    fscanf(fp, "InputFileName=%s\n",
-           config_ptr->InputFileName);
-
-    fscanf(fp, "Options=%d\n",
-           &config_ptr->Options);
-
-    fscanf(fp, "SectionName=%s\n",
-           config_ptr->SectionName);
-
-    fscanf(fp, "Address=%llx",
-           &config_ptr->Address);
-
-    fclose(fp);
-}
 
 int main(int argc, const char* argv[]) {
 
-    Config config;
+    if (argc < 2)
+        return 0;
 
-    config_parser(&config);
-
-    printf("config: %s %d %s %llu\n",
-           config.InputFileName,
-           config.Options,
-           config.SectionName,
-           config.Address);
+    FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("file does not exists");
+        return 1;
+    }
 
     return 0;
 }
