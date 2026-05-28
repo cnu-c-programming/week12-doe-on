883eb8fd24d9baee0f955224bc7d2e46d0607a70
doe-on <fovusshi01197@gmail.com>
2026-05-28 18:29:27 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,21 +1,23 @@
+
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-
     if (argc < 2)
         return 0;
 
-    const char* filenmae = argv[1];
-    char buffer[256] = {0};
+    for (int i = 1; i < argc; i++) {
+        const char* filename = argv[i];
+        char buffer[256] = {0};
 
-    FILE* fp = fopen(filename, "r");
-    if (fp == NULL)
-        return 0;
+        FILE* fp = fopen(filename, "r");
+        if (fp == NULL)
+            return 0;
 
-    fgets(buffer, sizedof(buffer), fp):
-    fclose(fp);
+        fgets(buffer, sizeof(buffer), fp);
+        fclose(fp);
 
-    printf("%s\n", buffer);
+        printf("%s\n", buffer);
+    }
 
     return 0;
 }
