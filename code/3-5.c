bc64afb72323556bbad12b2ac8ff3faf6aa505b3
doe-on <fovusshi01197@gmail.com>
2026-05-28 18:26:44 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,21 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    int value;
 
+    char str[256];
+    while (fscanf(fp, "%s", str) != EOF) {
+
+        if (sscanf(str, "%d", &value) == 1) {
+
+            sum += value;
+        }
+        else {
+
+            fprintf(stderr, "invalid input %s\n", str);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
