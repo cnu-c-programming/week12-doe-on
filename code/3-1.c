9a3198ac6da304c70fc119786900d11e04e696bc
doe-on <fovusshi01197@gmail.com>
2026-05-28 18:27:38 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-
     if (argc < 2)
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
     if (fp == NULL) {
-        perror("file does not exists");
+        perror("file dose not exists");
         return 1;
     }
 
