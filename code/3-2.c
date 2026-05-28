07b2abc5069aff496a42036251b0d85a9c4eeeff
doe-on <fovusshi01197@gmail.com>
2026-05-28 18:19:27 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    
+
     if (argc < 2)
         return 0;
-    
+
     const char* filename = argv[1];
     char buffer[10] = {0};
 
-    FILE* fp = fopen(filename, "r");
+    FILE* fp = fopen(filename, "r");=
+    if (fp == NULL)
+        return 0;
+    fscanf(fp, "%9s", buffer);
+    fclose(fp);
 
+    printf("%s\n", buffer);
 
     return 0;
 }
-
